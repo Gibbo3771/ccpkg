bac5f58b176307b6e352bc5b705790c3ddaf47a2
Stephen Gibson <gibson.stephen92@gmail.com>
2020-11-13 18:51:04 +0000

    Function to detect if folder contains a project, gitignore added

--- mcpkg.lua
+++ mcpkg.lua
@@ -1,4 +1,3 @@
-package.path = package.path .. ";" .. "/vendor/?;vendor/?.lua"
 local params = {...}
 
 if(table.getn(params) == 0) then
@@ -12,7 +11,11 @@ local arg = params[2]
 
 local path = fs.getDir(shell.getRunningProgram())
 local vendorPath = fs.getDir(shell.getRunningProgram()).."/vendor"
+local tmpPath = fs.getDir(shell.getRunningProgram()).."/tmp"
 
+local function isProjectFolder()
+    return fs.exists(path.."/pkg.json")
+end
 
 local function init()
     
@@ -35,8 +38,32 @@ local function init()
 end
 
 local function install()
-    print("Fetching master list...")
-    print("Installing "..arg.."...")
+    
+    function getFormula()
+        local req = http.get("https://github.com/Gibbo3771/pkgmc/blob/main/formula/"..arg)
+        if(not req) then
+            error("Could not download formula") 
+        end
+        local fh, err = io.open(tmpPath.."/"..arg, "w")
+        if(err) then
+            printError("Could not create pkg file")
+            error(err) 
+        end
+        fh:write(req.readAll())
+        io.close(fh)
+    end
+    
+    function download()
+        local zip = http.get("https://github.com/Gibbo3771/pkgmc/blob/main/mcpkg.lua")
+        
+    end
+    
+    function install()
+        print("Installing "..arg.."...")
+        
+    end
+    
+    getFormula()
 end
 
 
@@ -46,7 +73,7 @@ if(command == "init") then
         printError("You must specify a project name as the second argument when using init")
         return
     end
-    if(fs.exists(path.."/pkg.json")) then
+    if(isProjectFolder()) then
         printError("A project has already been created in this directory")
         return
     end
@@ -54,6 +81,10 @@ if(command == "init") then
     print("Finished, happy coding!")
     return
 elseif(command == "install") then
+    if(not isProjectFolder()) then
+        printError("Not in a project directory, run 'mcpkg init <project-name>' before trying to install packages")
+        return
+    end
     if(not arg) then
         printError("Pass the name of the package you want to install")
         return
