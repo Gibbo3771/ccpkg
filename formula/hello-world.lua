4417d37ab8e5f4b6b97a9d548ed3ef6854197d81
Stephen Gibson <gibson.stephen92@gmail.com>
2020-11-14 23:47:00 +0000

    formula name and repo update

--- formula/hello-world.lua
+++ formula/hello-world.lua
@@ -1,9 +1,9 @@
 local helloWorld = {
-    name = "hello-world",
+    name = "ccpkg-hello-world",
     description = "This is an example formula",
     versions = {
-        latest = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/2.0.0.tar.gz",
-        ["1.0.0"] = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/1.0.0.tar.gz"
+        latest = "https://github.com/Gibbo3771/ccpkg-hello-world/archive/2.0.0.tar.gz",
+        ["1.0.0"] = "https://github.com/Gibbo3771/ccpkg-hello-world/archive/1.0.0.tar.gz"
     }
 }
 
