45cdb39d785d98177f2eab36e517112a96a0b369
Stephen Gibson <gibson.stephen92@gmail.com>
2020-11-14 17:33:58 +0000

    Updated formula to point to zip

--- formula/hello-world.lua
+++ formula/hello-world.lua
@@ -2,8 +2,8 @@ local helloWorld = {
     name = "hello-world",
     description = "This is an example formula",
     versions = {
-        latest = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/2.0.0.tar.gz",
-        ["1.0.0"] = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/1.0.0.tar.gz"
+        latest = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/2.0.0.zip",
+        ["1.0.0"] = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/1.0.0.zip"
     }
 }
 
