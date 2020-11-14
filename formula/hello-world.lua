f97b6866f3a8614539a63a61fb689d019e2ce25f
Stephen Gibson <gibson.stephen92@gmail.com>
2020-11-14 02:31:44 +0000

    formula no longer in charge of downloading

--- formula/hello-world.lua
+++ formula/hello-world.lua
@@ -6,10 +6,6 @@ local helloWorld = {
         ["1.0.0"] = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/1.0.0.tar.gz"
     }
 }
--- Download the formula
-function helloWorld:download()
-    
-end
 
 -- Install the formula
 function helloWorld:install()
