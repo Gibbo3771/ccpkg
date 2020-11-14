4ce2986ca60675c1e9d8d25d0096d342e05b8bc6
Stephen Gibson <gibson.stephen92@gmail.com>
2020-11-14 02:30:18 +0000

    Added 2 version to hello world for testing

--- formula/hello-world.lua
+++ formula/hello-world.lua
@@ -1,9 +1,11 @@
 local helloWorld = {
     name = "hello-world",
     description = "This is an example formula",
-    versions = {"latest" = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/1.0.0.tar.gz"}
+    versions = {
+        latest = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/2.0.0.tar.gz",
+        ["1.0.0"] = "https://github.com/Gibbo3771/mcpkg-hello-world/archive/1.0.0.tar.gz"
+    }
 }
-
 -- Download the formula
 function helloWorld:download()
     
