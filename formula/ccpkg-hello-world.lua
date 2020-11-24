de0b5b635e19c1969ad2e383bf715b794db5bfcd
Stephen Gibson <gibson.stephen92@gmail.com>
2020-11-24 17:12:26 +0000

    uninstall hook fixed, some more keys for formula

--- formula/ccpkg-hello-world.lua
+++ formula/ccpkg-hello-world.lua
@@ -4,6 +4,8 @@ local package = { -- [REQUIRED] The table that ccpkg will require when it downlo
     homepage = "https://github.com/Gibbo3771/ccpkg-hello-world", -- [OPTIONAL] The homepage for the package
     repository = "https://github.com/Gibbo3771/ccpkg-hello-world", -- [OPTIONAL] The repository for the package
     authors = { "Stephen Gibson" }, -- [OPTIONAL] An array of authors for this package
+    contributors = { },  -- [OPTIONAL] An array of contributors to this package
+    license = "MIT",  -- [RECOMMENDED] The license that this package lives under
     globalOnly = false,  -- [OPTIONAL] If this package can only be installed globally, defaults to false
     versions = { -- [REQUIRED] A table of the available versions, where the key is the version number and the value is the download url
         ["2.0.0"] = "https://github.com/Gibbo3771/ccpkg-hello-world/archive/2.0.0.tar.gz",
@@ -16,24 +18,32 @@ function package:stable() -- [REQUIRED] Specifies the "stable" version of the pa
 end
 
 -- TODO
--- This hook is called right after the package has been installed
-function package:postInstall(version)  -- [OPTIONAL] A post install hook
+-- This hook is called right before the package is installed
+function package:preInstall(version)  -- [OPTIONAL] A pre install hook
     -- Do some stuff
 end
 
 -- This hook is called during the installation of the package, if it
 -- exists on the formula, this will be ran instead of ccpkg install
 -- code, allowing custom installations
---function package:install(ccpkg, artifacts, version)  -- [OPTIONAL] An installation hook. Allows custom logic for installation
---    -- As an example, we don't actually do anything custom, we just include
---    -- the package as normal
---    ccpkg.include(artifacts, self.name, version)
---end
+function package:install(ccpkg, artifacts, version)  -- [OPTIONAL] An installation hook. Allows custom logic for installation
+    -- As an example, we don't actually do anything custom, we just include
+    -- the package as normal
+    ccpkg.include(artifacts, self.name, version)
+end
 
 -- TODO
--- This hook is called right before the package is installed
-function package:preInstall(version)  -- [OPTIONAL] A pre install hook
+-- This hook is called right after the package has been installed
+function package:postInstall(version)  -- [OPTIONAL] A post install hook
     -- Do some stuff
+end
+
+-- This hook is called during the uninstallation of a package
+-- code, allowing custom installations
+function package:uninstall(ccpkg, artifacts, version)  -- [OPTIONAL] An uninstallation hook. Allows custom logic for uninstallation
+    -- As an example, we don't actually do anything custom, we just remove
+    -- the package as normal
+    ccpkg.remove(artifacts, self.name, version)
 end
 
 return package  -- [REQUIRED] Always return your package table
